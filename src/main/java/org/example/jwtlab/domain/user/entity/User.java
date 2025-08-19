b06c67260a2ebd025170d51a9815b9f72083b03a
dawn0920 <saoul0920@gmail.com>
2025-08-19 23:25:21 +0900

    feat: User 엔티티 및 UserRole enum 구현

---
+++ src/main/java/org/example/jwtlab/domain/user/entity/User.java
@@ -0,0 +1,19 @@
+package org.example.jwtlab.domain.user.entity;
+
+import lombok.*;
+import org.example.jwtlab.domain.user.enums.UserRole;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    public static User builder;
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private UserRole userRole;
+
+    public void updateRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+}
---
+++ src/main/java/org/example/jwtlab/domain/user/enums/UserRole.java
@@ -0,0 +1,17 @@
+package org.example.jwtlab.domain.user.enums;
+
+import com.sun.jdi.request.InvalidRequestStateException;
+
+import java.util.Arrays;
+
+public enum UserRole {
+    USER,
+    ADMIN;
+
+    public static UserRole of(String role) {
+        return Arrays.stream(UserRole.values())
+                .filter(r -> r.name().equalsIgnoreCase(role))
+                .findFirst()
+                .orElseThrow(() -> new InvalidRequestStateException("유효하지 않은 접근입니다."));
+    }
+}
