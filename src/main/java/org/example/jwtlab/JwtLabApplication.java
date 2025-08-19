691f88fa7a228584e7990688ec1fc23cc553763a
dawn0920 <saoul0920@gmail.com>
2025-08-19 16:26:51 +0900

    feat: JWTLab 프로젝트 초기 생성

---
+++ src/main/java/org/example/jwtlab/JwtLabApplication.java
@@ -0,0 +1,13 @@
+package org.example.jwtlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLabApplication.class, args);
+    }
+
+}
