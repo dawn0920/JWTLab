1222818979982e62df5f33ea2bc74ca1d9bcc5c1
dawn0920 <saoul0920@gmail.com>
2025-08-20 21:13:16 +0900

    refactor: UserController 인증 관련 API 전용으로 리팩토링

--- src/main/java/org/example/jwtlab/domain/user/controller/UserController.java
+++ src/main/java/org/example/jwtlab/domain/user/controller/UserController.java
@@ -11,28 +11,20 @@ import lombok.RequiredArgsConstructor;
 import org.example.jwtlab.domain.auth.response.TokenResponse;
 import org.example.jwtlab.domain.user.dto.request.LoginRequest;
 import org.example.jwtlab.domain.user.dto.request.SignupRequest;
-import org.example.jwtlab.domain.user.dto.request.UpdateUserRoleRequest;
 import org.example.jwtlab.domain.user.dto.response.SignupResponse;
-import org.example.jwtlab.domain.user.dto.response.UpdateUserRoleResponse;
-import org.example.jwtlab.domain.user.entity.User;
-import org.example.jwtlab.domain.user.exception.UserErrorCode;
-import org.example.jwtlab.domain.user.exception.UserException;
 import org.example.jwtlab.domain.user.repository.UserRepository;
 import org.example.jwtlab.domain.user.service.UserService;
-import org.example.jwtlab.global.jwt.UserAuth;
 import org.example.jwtlab.global.response.BaseResponse;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 @Tag(name = "User API", description = "유저 관련 API")
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @Operation(
             summary = "회원가입",
@@ -47,7 +39,7 @@ public class UserController {
                                     )))
             }
     )
-    @PostMapping("/auth/signup")
+    @PostMapping("/signup")
     public BaseResponse<SignupResponse> signup(
             @Valid @RequestBody SignupRequest signupRequest
             ) {
@@ -74,62 +66,11 @@ public class UserController {
                                     }))
             }
     )
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public BaseResponse<TokenResponse> login(@RequestBody LoginRequest loginRequest,
                                              HttpServletResponse response) {
         TokenResponse tokenResponse = userService.login(loginRequest, response);
         return new BaseResponse<>(tokenResponse);
     }
-
-    @Operation(
-            summary = "권한 변경",
-            description = "관리자 권한을 가진 사용자가 다른 유저의 권한을 변경합니다.",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "권한 변경 성공"),
-                    @ApiResponse(responseCode = "403", description = "권한 없음",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    examples = @ExampleObject(
-                                            name = "accessDeniedError",
-                                            value = "{\"status\": 403, \"code\": \"ACCESS_DENIED\", \"message\": \"접근 권한이 없습니다.\"}"
-                                    ))),
-                    @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음",
-                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    examples = @ExampleObject(
-                                            name = "userNotFoundError",
-                                            value = "{\"status\": 404, \"code\": \"U001\", \"message\": \"존재하지 않는 사용자 입니다.\"}"
-                                    )))
-            }
-    )
-    @PatchMapping("/admin/user/{targetUserId}/roles")
-    public BaseResponse<UpdateUserRoleResponse> updateUserRole(
-            @PathVariable Long targetUserId,
-            @AuthenticationPrincipal UserAuth userAuth
-    ) {
-        User targetUser = userRepository.findById(targetUserId).orElseThrow(
-                () -> new UserException(UserErrorCode.USER_ID_NOT_FOUND)
-        );
-
-        Long authUserId = userAuth.getUserId();
-        UpdateUserRoleResponse response = userService.updateUserRole(authUserId, targetUserId);
-        return new BaseResponse<>(response);
-    }
-
-    /* 권한 변경 테스트용 (USER, ADMIN 모두 변경 가능)
-    @PatchMapping("/admin/user/{targetUserId}/roles")
-    public BaseResponse<UpdateUserRoleResponse> updateUserRole(
-            @PathVariable Long targetUserId,
-            @RequestBody @Valid UpdateUserRoleRequest updateUserRoleRequest,
-            @AuthenticationPrincipal UserAuth userAuth
-    ) {
-        User targetUser = userRepository.findById(targetUserId).orElseThrow(
-                () -> new UserException(UserErrorCode.USER_ID_NOT_FOUND)
-        );
-
-        Long authUserId = userAuth.getUserId();
-        UpdateUserRoleResponse response = userService.updateUserRole(authUserId, targetUserId, updateUserRoleRequest);
-        return new BaseResponse<>(response);
-    }
-
-     */
 
 }
